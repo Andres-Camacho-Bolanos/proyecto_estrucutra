9437c2f0e5e46734014331c197db2d56a1a78ce3
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2023-11-04 18:42:32 -0600

    Update estructura_base.c
    Implementación de funciones al código base

--- estructura_base.c
+++ estructura_base.c
@@ -54,22 +54,223 @@ typedef struct TreeNode {
 
 // Función para agregar una actividad a la lista circular
 void Actividades(Node** head, Actividad) {
-	// Implementa la lógica para agregar la actividad a la lista
-}
+	void Actividades(Node* head, Actividad actividad) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->dato = actividad;
 
-// Función para mostrar las actividades en la lista
-void mostrarActividades(Node* head) {
-	// Implementa la lógica para recorrer y mostrar las actividades
+    if (head == NULL) {
+        newNode->next = newNode;
+        newNode->prev = newNode;
+        return newNode;
+    }
+
+    Node* last = head->prev;
+
+    newNode->next = head;
+    head->prev = newNode;
+
+    newNode->prev = last;
+    last->next = newNode;
+
+    return head;
+}
 }
 
 // Función para añadir una alarma a las actividades por realizar
 void sistemaAlarma(Node* head, Actividad) {
-  // Implementar lógica para añadir un sistema recordatorio para las actividades seleccionadas
+// Inicializar una cola
+Cola* inicializarCola(int capacidad) {
+    Cola* cola = (Cola*)malloc(sizeof(Cola));
+    cola->capacidad = capacidad;
+    cola->elementos = (Alarma*)malloc(sizeof(Alarma) * capacidad);
+    cola->frente = cola->fin = -1;
+    return cola;
+}
+
+// Verificar si la cola está vacía
+int estaVacia(Cola* cola) {
+    return cola->frente == -1;
+}
+
+// Verificar si la cola está llena
+int estaLlena(Cola* cola) {
+    return (cola->fin + 1) % cola->capacidad == cola->frente;
+}
+
+// Agregar una alarma a la cola
+void agregarAlarma(Cola* cola, Alarma alarma) {
+    if (estaLlena(cola)) {
+        printf("La cola de alarmas está llena. No se puede agregar más alarmas.\n");
+        return;
+    }
+
+    if (estaVacia(cola)) {
+        cola->frente = 0;
+    }
+
+    cola->fin = (cola->fin + 1) % cola->capacidad;
+    cola->elementos[cola->fin] = alarma;
+}
+
+// Eliminar una alarma de la cola (cuando se dispara)
+void eliminarAlarma(Cola* cola) {
+    if (estaVacia(cola)) {
+        printf("No hay alarmas para eliminar.\n");
+        return;
+    }
+
+    if (cola->frente == cola->fin) {
+        cola->frente = cola->fin = -1;
+    } else {
+        cola->frente = (cola->frente + 1) % cola->capacidad;
+    }
+}
+
+// Mostrar las alarmas en la cola
+void mostrarAlarmas(Cola* cola) {
+    if (estaVacia(cola)) {
+        printf("La cola de alarmas está vacía.\n");
+        return;
+    }
+
+    int i = cola->frente;
+    printf("Alarmas programadas:\n");
+    while (i != cola->fin) {
+        printf("Actividad: %s\n", cola->elementos[i].actividad.nombre);
+        i = (i + 1) % cola->capacidad;
+    }
+    printf("Actividad: %s\n", cola->elementos[cola->fin].actividad.nombre);
+}
 }
 
 // Función para aladir un marcador a las actividades seleccionadas
 void marcadorActividad(Node* head, Actividad) {
-  // Impelementa la lógica para añadir un valor extra de prioridad a la actividad
+// Inicializar una pila
+Pila* inicializarPila(int capacidad) {
+    Pila* pila = (Pila*)malloc(sizeof(Pila));
+    pila->top = NULL;
+    pila->capacidad = capacidad;
+    pila->tamaño = 0;
+    return pila;
+}
+
+// Verificar si la pila está vacía
+int estaVacia(Pila* pila) {
+    return pila->top == NULL;
+}
+
+// Agregar un elemento (actividad marcada) a la pila
+void push(Pila* pila, Actividad actividad) {
+    if (pila->tamaño >= pila->capacidad) {
+        printf("La pila de marcadores está llena. No se pueden agregar más marcadores.\n");
+        return;
+    }
+
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->dato = actividad;
+    newNode->next = pila->top;
+    pila->top = newNode;
+    pila->tamaño++;
+}
+
+// Obtener y eliminar el elemento superior de la pila
+Actividad pop(Pila* pila) {
+    if (estaVacia(pila)) {
+        printf("La pila de marcadores está vacía.\n");
+        Actividad actividadVacia;
+        actividadVacia.id = -1;
+        return actividadVacia;
+    }
+
+    Node* temp = pila->top;
+    Actividad actividadMarcada = temp->dato;
+    pila->top = pila->top->next;
+    free(temp);
+    pila->tamaño--;
+
+    return actividadMarcada;
+}
+
+void prioridadGrafos(Node* head, Actividad) {
+	Grafo* crearGrafo(Actividad actividades[], int numActividades) {
+    Grafo* grafo = (Grafo*)malloc(sizeof(Grafo));
+    grafo->numNodos = numActividades;
+    grafo->matrizAdyacencia = (int**)malloc(numActividades * sizeof(int*));
+
+    for (int i = 0; i < numActividades; i++) {
+        grafo->matrizAdyacencia[i] = (int*)malloc(numActividades * sizeof(int));
+
+        for (int j = 0; j < numActividades; j++) {
+            if (i == j) {
+                grafo->matrizAdyacencia[i][j] = 0;
+            } else if (actividades[i].prioridad > actividades[j].prioridad) {
+                grafo->matrizAdyacencia[i][j] = 1;
+            } else {
+                grafo->matrizAdyacencia[i][j] = 0;
+            }
+        }
+    }
+
+    return grafo;
+}
+
+// Función para realizar una búsqueda topológica en el grafo
+void topologicalSortUtil(Grafo* grafo, int v, int visitado[], Node* actividades[], int* index) {
+    visitado[v] = 1;
+    for (int i = 0; i < grafo->numNodos; i++) {
+        if (grafo->matrizAdyacencia[v][i] && !visitado[i]) {
+            topologicalSortUtil(grafo, i, visitado, actividades, index);
+        }
+    }
+    actividades[(*index)++] = agregarActividad(NULL, actividades[v]->dato);
+}
+
+// Función que llama a la búsqueda topológica
+Node** topologicalSort(Grafo* grafo, Node* actividades[], int numActividades) {
+    int* visitado = (int*)malloc(numActividades * sizeof(int));
+    int index = 0;
+    Node** ordenRecomendado = (Node**)malloc(numActividades * sizeof(Node*));
+
+    for (int i = 0; i < numActividades; i++) {
+        visitado[i] = 0;
+    }
+
+    for (int i = 0; i < numActividades; i++) {
+        if (!visitado[i]) {
+            topologicalSortUtil(grafo, i, visitado, actividades, &index);
+        }
+    }
+
+    return ordenRecomendado;
+}
+}
+
+void relojCuentafinal(Node* head, actividad) {
+// Inicializar un nodo de árbol
+TreeNode* inicializarNodo(int hora) {
+    TreeNode* nodo = (TreeNode*)malloc(sizeof(TreeNode));
+    nodo->hora = hora;
+    nodo->actividades = NULL;
+    nodo->izquierda = NULL;
+    nodo->derecha = NULL;
+    return nodo;
+}
+
+// Agregar una actividad programada a un nodo de árbol
+void agregarActividadProgramada(TreeNode* nodo, Actividad actividad) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->dato = actividad;
+    newNode->next = nodo->actividades;
+    nodo->actividades = newNode;
+}
+
+// Función para aumentar una hora en el reloj
+void aumentarHora(int* hora) {
+    (*hora)++;
+    if (*hora > 23) {
+        *hora = 0;
+    }
+}
 }
 
 int main() {
