be378901ac7227cf3edab5696c27055eda626be4
bk201 <reao20137@gmail.com>
2023-11-30 03:05:38 -0600

    Creacion de archivos de cabecera con la explicacion general de las estructuras de datos

---
+++ queue.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void info_queue(){
+    printf("\n\nEn programación, una cola (queue) es una estructura de datos lineal que se basa en el principio FIFO (First In, First Out), es decir, el primer elemento en entrar es el primero en salir.\n");
+    printf("\nUna cola se comporta como una caja con varios elementos, en donde solo se pueden realizar dos operaciones: encolar (enqueue) y desencolar (dequeue). Al encolar un elemento, se agrega al final de la cola, y al desencolar se elimina el elemento que está en el frente de la cola.\n");
+    printf("\nEjemplo de uso: En sistemas operativos, la cola se utiliza para almacenar procesos en espera de ser ejecutados. En redes de computadoras, las colas se utilizan para almacenar paquetes en espera de ser transmitidos. En sistemas de impresión, las colas se utilizan para almacenar documentos en espera de ser impresos.\n");
+    printf("\nEn resumen, una cola es una estructura de datos en la que las operaciones de inserción se realizan en un extremo (final de la cola) y las operaciones de eliminación se realizan en otro extremo (frente de la cola). Es una estructura de datos esencial para la programación y es utilizada en una variedad de problemas y aplicaciones.");
+
+}
+
