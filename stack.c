e66f5b205afd8f9c78d25e30235e273508ab008f
Andres-Camacho-Bolanos <111325635+Andres-Camacho-Bolanos@users.noreply.github.com>
2023-11-30 22:28:40 -0600

    Create stack.c

---
+++ stack.c
@@ -0,0 +1,7 @@
+#include <stack.c>
+
+void info_stack(){
+    printf("En programación, una pila (stack) es una estructura de datos lineal que se basa en el principio LIFO (Last In, First Out),es decir, el último elemento en entrar es el primero en salir.\n");
+    printf("\nUna pila se comporta como una caja con varios elementos, en donde solo se pueden realizar dos operaciones: apilar (push) y desapilar (pop). Al apilar un elemento, se agrega al tope de la pila, y al desapilar se elimina el elemento que está en el tope de la pila.\n");
+    printf("\nEn resumen, una pila es una estructura de datos en la que las operaciones de inserción y eliminación se realizan solo en un extremo, conocido como 'tope' de la pila. Es una estructura de datos esencial para la programación y es utilizada en una variedad de problemas y aplicaciones.");    
+}
